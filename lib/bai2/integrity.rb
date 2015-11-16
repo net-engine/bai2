0c5fcae9a5ac04a752e26a908ca33236840b9b66
Jay Balanay <manolet@netengine.com.au>
2015-11-16 12:49:30 +0800

    Fix for checking sum of all transactions and account summaries vs total amount

--- lib/bai2/integrity.rb
+++ lib/bai2/integrity.rb
@@ -101,9 +101,9 @@ module Bai2
 
         # Check sum vs. summary + transaction sums
         actual_sum = self.transactions.map(&:amount).reduce(0, &:+) \
-          #+ self.summaries.map {|s| s[:amount] }.reduce(0, &:+)
-          # TODO: ^ there seems to be a disconnect between what the spec defines
-          # as the formula for the checksum and what SVB implements...
+          + self.summaries.map {|s| s[:amount] }.reduce(0, &:+)
+        # Use the formula specific to Westpac Integration to properly
+        # match the actual_sum vs the expected sum
 
         unless expectation[:sum] == actual_sum
           raise IntegrityError.new(
